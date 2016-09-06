9b21e684160fa8caaa99863389f8bf3da3456584
Roberto Ostinelli <roberto@ostinelli.net>
2016-09-06 20:33:28 +0200

    Remove unnecessary comment.

--- include/erlgate.hrl
+++ include/erlgate.hrl
@@ -23,7 +23,6 @@
 %% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 %% THE SOFTWARE.
 %% ==========================================================================================================
-%% specs
 
 -type transport_spec() :: tcp | {ssl, [ssl:ssl_option()]}.
 
